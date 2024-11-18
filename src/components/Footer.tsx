fc61d4e864768f940c69feefe3d097d2773265e7
Jens Bengtsson <jb225je@student.lnu.se>
2024-11-18 13:33:32 +0100

    added some content to header and footer. and set up settings for tailwindcss

---
+++ src/components/Footer.tsx
@@ -0,0 +1,15 @@
+export const Footer = () => {
+
+    return (
+        <footer>
+            <div>
+                <h2>Kontakta oss</h2>
+                <p>tele</p>
+                <p>email</p>
+            </div>
+            <div>
+                GIDI logo-img
+            </div>
+        </footer>
+    )
+}
---
+++ src/components/Header.tsx
@@ -0,0 +1,23 @@
+import styles from './header.module.css'
+// import { IoMenu } from "react-icons/io5";
+// import { LightDarkModeSet } from '../lightDarkModeSet/LightDarkModeSet';
+
+export const Header = () => {
+
+    return (
+        <header /* className={`${styles.header}`} */>
+            <div /* className={`btn ${styles.sidebar__menu}`} */>
+                {/* <IoMenu /> */}
+                <p>Hamburger dropdown menu</p>
+            </div>
+            <div /* className={`${styles.logo__container}` }*/>
+                GIDI
+            </div>
+            <div /* className={`flex ${styles.header__options}`} */>
+                {/* <LightDarkModeSet /> */}
+                <p>darkmode loggo</p>
+                <div className='btn'>logga in</div>
+            </div>
+        </header>
+    )
+}
