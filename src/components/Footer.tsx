a85044f9400f44012c4a292f10b71057105d176e
Jens Bengtsson <jb225je@student.lnu.se>
2025-01-02 22:49:01 +0100

    basic footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,15 +1,13 @@
 export const Footer = () => {
 
     return (
-        <footer className="flex flex-row justify-center">
+        <footer className="bg-lightmode-main-bg dark:bg-darkmode-main-bg text-lightmode-prim-text dark:text-darkmode-prim-text flex flex-row justify-between p-2 gap-2">
             <div>
                 <h2>Kontakta oss</h2>
-                <p>tele:</p>
-                <p>email:</p>
+                <p>Tele: 070 285 94 83</p>
+                <p>Email: Jens.bengtsson91@gmail.com</p>
             </div>
-            <div>
-                GIDI logo-img
-            </div>
+            <p>GIDI logo-img</p>
         </footer>
     )
 }
