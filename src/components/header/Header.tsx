b3800f50ced4e3aa556a4318b062908eef7bf8f5
Jens Bengtsson <jb225je@student.lnu.se>
2025-01-02 22:35:26 +0100

    dark/lightmode now works

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,12 +1,47 @@
-import { useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import { BrightnessDeviceSettingIcon, DarkmodeIcon, LightmodeIcon, MenuIcon } from "../../assets/icons/icons"
 
+type Theme = 'dark' | 'light' | 'system';
+
 export const Header = () => {
-    const [theme, setTheme] = useState('dark')
+    const themes: Theme[] = ['dark', 'light', 'system']
+
+    const [theme, setTheme] = useState<Theme>('dark')
     const [show, setShow] = useState(false)
     const [isLoggedIn, setIsLoggedIn] = useState(false)
 
-    const setThemeMode = (mode: string) => {
+    const themeMenuRef = useRef<HTMLDivElement>(null)
+
+    // changing theme without refreshing browser, and save to LS
+    useEffect(() => {
+        const root = document.documentElement;
+        if (theme === 'dark') {
+            root.classList.add('dark');
+            localStorage.setItem('theme', 'dark');
+        } else if (theme === 'light') {
+            root.classList.remove('dark');
+            localStorage.setItem('theme', 'light');
+        } else {
+            localStorage.removeItem('theme');
+            if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+                root.classList.add('dark');
+            } else {
+                root.classList.remove('dark');
+            }
+        }
+    }, [theme]);
+    
+    // closes theme dropdown
+    useEffect(() => {
+        const closeThemeDropDown = (e: MouseEvent): void => {
+            if (themeMenuRef.current && !themeMenuRef.current.contains(e.target as Node)) setShow(false)
+        }
+        show ? document.addEventListener('mousedown', closeThemeDropDown) : document.removeEventListener('mousedown', closeThemeDropDown)
+        return () => document.removeEventListener('mousedown', closeThemeDropDown)
+    }, [show])
+
+    // changes theme
+    const setThemeMode = (mode: Theme) => {
         setTheme(mode)
         setShow(false)
     }
@@ -20,29 +55,30 @@ export const Header = () => {
                 GIDI
             </h1>
             <div className="flex items-center p-4 gap-1">
-                <div className="relative">
-                    <button className="p-2 w-10 h-10 bg-gray-200 rounded-full"
-                        onClick={() => setShow(!show)} >
+                <div ref={themeMenuRef} className="relative">
+                    <button className="p-2 w-10 h-10 bg-gray-200 rounded-full" onClick={()=>setShow(!show)}>
                         {theme === 'dark' ? (<DarkmodeIcon />)
                             : theme === 'light' ? (<LightmodeIcon />)
                                 : <BrightnessDeviceSettingIcon />}
                     </button>
-                    {show &&
+                    {show && (
                         <ul className="absolute right-0 top-full mt-2 bg-white border w-fit rounded-lg">
-                            <li className="flex w-100 m-1 hover:cursor-pointer hover:bg-gray-200" onClick={() => setThemeMode('dark')}>
-                                <button className='w-6 h-6'><DarkmodeIcon /></button>
-                                <p className="ml-2 whitespace-nowrap">Mörkt läge</p>
-                            </li>
-                            <li className="flex w-100 m-1 hover:cursor-pointer hover:bg-gray-200" onClick={() => setThemeMode('light')}>
-                                <button className='w-6 h-6'><LightmodeIcon /></button>
-                                <p className="ml-2 whitespace-nowrap">Ljust läge</p>
-                            </li>
-                            <li className="flex w-100 m-1 hover:cursor-pointer hover:bg-gray-200" onClick={() => setThemeMode('system')}>
-                                <button className='w-6 h-6'><BrightnessDeviceSettingIcon /></button>
-                                <p className="ml-2 whitespace-nowrap">Systemets läge</p>
-                            </li>
+                            {themes.map((mode) => (
+                                <li key={mode} className="flex w-100 m-1 hover:cursor-pointer hover:bg-gray-200" onClick={() => setThemeMode(mode)}>
+                                    <button className='w-6 h-6'>
+                                        {mode === 'dark' && <DarkmodeIcon />}
+                                        {mode === 'light' && <LightmodeIcon />}
+                                        {mode === 'system' && <BrightnessDeviceSettingIcon />}
+                                    </button>
+                                    <p className="ml-2 whitespace-nowrap">
+                                        {mode === 'dark' && 'Mörkt läge'}
+                                        {mode === 'light' && 'Ljust läge'}
+                                        {mode === 'system' && 'Systemläge'}
+                                    </p>
+                                </li>
+                            ))}
                         </ul>
-                    }
+                    )}
                 </div>
                 <div className=''>logga {isLoggedIn ? 'ut' : 'in'}</div>
             </div>
